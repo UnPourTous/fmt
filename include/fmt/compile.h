f51f1beb7e662eb1df96143f10ffef941d17bb44
Pritesh Nandgaonkar <prit91@fb.com>
2021-04-28 17:21:29 +0100

    try a fix

--- include/fmt/compile.h
+++ include/fmt/compile.h
@@ -114,7 +114,12 @@ struct is_compiled_string : std::is_base_of<compiled_string, S> {};
     std::string s = fmt::format(FMT_COMPILE("{}"), 42);
   \endrst
  */
-#define FMT_COMPILE(s) FMT_STRING_IMPL(s, fmt::detail::compiled_string)
+// #define FMT_COMPILE(s) FMT_STRING_IMPL(s, fmt::detail::compiled_string)
+#ifdef __cpp_if_constexpr
+#  define FMT_COMPILE(s) FMT_STRING_IMPL(s, fmt::detail::compiled_string)
+#else
+#  define FMT_COMPILE(s) FMT_STRING(s)
+#endif
 
 #if FMT_USE_NONTYPE_TEMPLATE_PARAMETERS
 template <typename Char, size_t N, fixed_string<Char, N> Str>
@@ -901,8 +906,7 @@ FMT_INLINE std::basic_string<typename S::char_type> format(const S&,
     }
   }
 #endif
-  // detail::compile(S());
-  constexpr auto compiled = detail::compiled_format<S, Args...>(detail::to_string_view(S()));
+  constexpr auto compiled = detail::compile<Args...>(S());
 #ifdef __cpp_if_constexpr
   if constexpr (std::is_same<remove_cvref_t<decltype(compiled)>,
                              detail::unknown_format>()) {
